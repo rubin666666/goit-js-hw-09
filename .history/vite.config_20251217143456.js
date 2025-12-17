747d5c9041057f2899edc9809eb32e7c59a28862
rubin <rubin666666666@gmail.com>
2025-12-17 14:39:01 +0200

    Add Google Fonts, fix paths to relative, adjust gallery gap to 24px

---
+++ .history/vite.config_20251217143456.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({  root: 'src',
+  define: {
+    global: 'window',
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '/src': path.resolve(__dirname, 'src'),
+    },
+  },
+});
