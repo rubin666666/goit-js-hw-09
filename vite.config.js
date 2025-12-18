c6803df6b3af8390842870f9cc0277df3361eae0
rubin <rubin666666666@gmail.com>
2025-12-19 00:58:37 +0200

    Fix vite.config.js for proper GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,41 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig({  root: 'src',
-  define: {
-    global: 'window',
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: glob.sync('./src/*.html'),
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '/src': path.resolve(__dirname, 'src'),
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
     },
-  },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
